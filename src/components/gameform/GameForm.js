c8685d32e6aca93089c7fa8a03e7ff6bc35aeca5
Bendzso04 <draskoczy.bendzso@gmail.com>
2024-06-04 22:31:59 +0100

    added form

--- src/components/gameform/GameForm.js
+++ src/components/gameform/GameForm.js
@@ -1,64 +1,45 @@
+"use client";
+import { useState } from "react";
+
 const GameForm = () => {
+  const [title, setTitle] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [release, setRelease] = useState("");
+
   return (
     <div>
-      <form
-        action="/submit"
-        method="post"
-        enctype="multipart/form-data"
-        className="bg-[#283347] p-6 rounded-lg shadow-md"
-      >
+      <form onSubmit={""} className="bg-[#283347] p-6 rounded-lg shadow-md">
         <div className="mb-4">
-          <label for="title" className="block text-white-700 font-bold mb-2">
-            Title:
-          </label>
+          <label className="block text-white-700 font-bold mb-2">Title:</label>
           <input
             type="text"
-            id="title"
-            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             required
           />
         </div>
         <div className="mb-4">
-          <label
-            for="publisher"
-            className="block text-white-700 font-bold mb-2"
-          >
+          <label className="block text-white-700 font-bold mb-2">
             Publisher:
           </label>
           <input
             type="text"
-            id="publisher"
-            name="publisher"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            required
-          />
-        </div>
-        <div class="mb-4">
-          <label
-            for="release_date"
-            className="block text-white-700 font-bold mb-2"
-          >
-            Release Date:
-          </label>
-          <input
-            type="date"
-            id="release_date"
-            name="release_date"
+            value={publisher}
+            onChange={(e) => setPublisher(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             required
           />
         </div>
         <div className="mb-4">
-          <label for="image" className="block text-white-700 font-bold mb-2">
-            Image:
+          <label className="block text-white-700 font-bold mb-2">
+            Release Date:
           </label>
           <input
-            type="file"
-            id="image"
-            name="image"
-            accept="image/*"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-white-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="date"
+            value={release}
+            onChange={(e) => setRelease(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             required
           />
         </div>
